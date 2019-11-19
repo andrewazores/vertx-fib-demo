f01c1cca7aecb6b3f78ef06f976e71fc53f5ce23
Andrew Azores <me@andrewazor.es>
2019-11-19 13:21:57 -0500

    Add run script

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -x
+
+function cleanup() {
+    set +e
+    # TODO: better container management
+    docker kill $(docker ps -a -q --filter ancestor=quay.io/andrewazores/vertx-fib-demo)
+    docker rm $(docker ps -a -q --filter ancestor=quay.io/andrewazores/vertx-fib-demo)
+}
+
+cleanup
+trap cleanup EXIT
+
+set -e
+
+docker run \
+    -p 8080:8080 \
+    --net container-jfr \
+    --hostname vertx-fib-demo \
+    --name vertx-fib-demo \
+    --memory 80M \
+    --cpus 1.0 \
+    --rm -it quay.io/andrewazores/vertx-fib-demo:latest "$@"
