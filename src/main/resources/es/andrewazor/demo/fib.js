5cec9dd8b48416aaffef192bbd62ecc3e3758e44
Andrew Azores <me@andrewazor.es>
2019-11-19 13:13:36 -0500

    Implement automatic filling of values table

--- src/main/resources/es/andrewazor/demo/fib.js
+++ src/main/resources/es/andrewazor/demo/fib.js
@@ -1,10 +1,49 @@
-function fib() {
-  var i = $("input").val();
+function fib(i, onresult, oncomplete) {
   $.ajax("/fib/" + i)
     .done(function(res) {
-      $("table").append("<tr><td>" + i + "</td><td>" + res + "</td></tr>");
+      if (onresult) {
+        onresult.call(this, res);
+      }
     })
     .fail(function(err) {
       alert(JSON.stringify(err));
+    })
+    .always(function() {
+      if (oncomplete) {
+        oncomplete.apply()
+      }
     });
 }
+
+function doFib(i, oncomplete) {
+  fib(i, function(res) { $("table").append("<tr><td>" + i + "</td><td>" + res + "</td></tr>"); }, oncomplete);
+}
+
+function startAuto() {
+  if (window.runAuto) {
+    return;
+  }
+  window.runAuto = true;
+  var i = 0;
+  var next = function() {
+    if (!window.runAuto) {
+      return;
+    }
+    var delay = 1 * 1000;
+    doFib(i, function() { setTimeout(next, delay); });
+    i++;
+  }
+  next.apply();
+}
+
+function stopAuto() {
+  window.runAuto = false;
+  $("table").empty();
+  setTableHeaders();
+}
+
+function setTableHeaders() {
+  $("table").append("<tr><th>Index</th><th>Value</th></tr>");
+}
+
+$(document).ready(setTableHeaders);
