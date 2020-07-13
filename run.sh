b8a08912f83a9f9ee4ac572a7fd4465d8c575fe2
Andrew Azores <me@andrewazor.es>
2020-07-13 15:25:21 -0400

    Use podman, optionally run in container-jfr pod

--- run.sh
+++ run.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 
 set -x
-
-function cleanup() {
-    set +e
-    # TODO: better container management
-    docker kill $(docker ps -a -q --filter ancestor=quay.io/andrewazores/vertx-fib-demo)
-    docker rm $(docker ps -a -q --filter ancestor=quay.io/andrewazores/vertx-fib-demo)
-}
-
-cleanup
-trap cleanup EXIT
-
 set -e
 
-docker run \
-    -p 8080:8080 \
-    --net container-jfr \
-    --hostname vertx-fib-demo \
-    --name vertx-fib-demo \
-    --memory 256M \
-    --cpus 0.5 \
-    --rm -it quay.io/andrewazores/vertx-fib-demo:latest "$@"
+ARGS=(--name vertx-fib-demo -p 8080:8080 -p 9093:9093)
+
+if podman pod exists container-jfr; then
+    ARGS+=(--pod container-jfr)
+fi
+
+ARGS+=(--rm -it quay.io/andrewazores/vertx-fib-demo:latest)
+
+podman run "${ARGS[@]}"
