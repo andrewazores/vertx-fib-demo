db25a4572eae0a66378cc760d9721b44a4525c7f
Janelle Law <jalaw@redhat.com>
2021-05-27 17:10:43 -0400

    Add default value for HTTP_PORT

--- run.sh
+++ run.sh
@@ -5,10 +5,14 @@ set -e
 
 ARGS=(--name vertx-fib-demo -p $HTTP_PORT -p 9093:9093)
 
+if [ -z "$HTTP_PORT" ]; then
+    HTTP_PORT=8080
+fi
+
 if podman pod exists container-jfr; then
     ARGS+=(--pod container-jfr)
 fi
 
 ARGS+=(--rm -it quay.io/andrewazores/vertx-fib-demo:latest)
 
-podman run "${ARGS[@]}"
+podman run --env HTTP_PORT="$HTTP_PORT" "${ARGS[@]}"
